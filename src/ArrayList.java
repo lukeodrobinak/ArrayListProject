f35943d8fbadcf29181871485cd69ca65b4a89d4
Zenitore <eayers25@kentdenver.org>
2023-10-11 09:09:02 -0600

    Added the method of boolean add(T data)

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -5,19 +5,28 @@
  * Collaborators:
  * Citations: Google, Oracle Java Docs, GitHub Documentation, Stack Overflow
  **/
-    public class ArrayList<T>{
+    public class ArrayList<T> {
 
-        private T value;
+    private T value;
+    private Node<T> rootNode = null;
 
 
-        public ArrayList() {//Default constructor
+    public ArrayList() {//Default constructor
+    }
+
+    public boolean add(T data) {
+        Node<T> currentNode = rootNode;
+        if (rootNode.equals(null)) {
+            rootNode = new Node<>(data);
+        } else {
+            while (currentNode.getChildNode().equals(null)) {
+                currentNode = currentNode.getChildNode();
+            }
+            Node<T> addedNode = new Node<>(data, currentNode, null);
+            currentNode.setChildNode(addedNode);
         }
-        public boolean add(T data){
-
-                Node<T> addedNode = new Node<>(data);
-
-            return true;
-        }
+        return true;
+    }
 }
 
 
